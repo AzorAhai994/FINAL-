b37965fd21fce11d7668f2c87dff8ab5fe89979d
Marinko Brdarević <39567693+AzorAhai994@users.noreply.github.com>
2021-01-30 00:50:54 +0100

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,9 +1,9 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('fox-store').then((cache) => cache.addAll([
-      './',
-  './index.html',
-'./style.css',
+      '../',
+  '../index.html',
+'../style.css',
     ])),
   );
 });
