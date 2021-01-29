99fb126f9e0b2a3764b65712e0546b93a48688b3
Marinko Brdarević <39567693+AzorAhai994@users.noreply.github.com>
2021-01-30 00:46:46 +0100

    Update global.js

--- js/global.js
+++ js/global.js
@@ -154,12 +154,3 @@
 
         ]);
     </script>
-
- <script>
-if ('serviceWorker'in navigator) {
-    navigator.serviceWorker.register('sw.js').then(()=>{
-        console.log('Service Worker Registered');
-    }
-    );
-}
- </script>
